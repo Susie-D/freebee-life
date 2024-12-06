dc25134a79ec6bab1e11e28f0cfe46ec75e8af79
Susie Douang <soutsada.d@gmail.com>
2024-12-05 20:26:43 -0600

    relocate index.html

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig(() => {
     return {
         build: { manifest: true, outDir: path.join(__dirname, 'build') },
+        root: path.join(__dirname, 'src'),
         server: {
             proxy: {
                 "/api": 'http://localhost:5001',
